7ce768089f457c5f327f78fa2ef55bcb262f1398
apurva <apurva@DESKTOP-K1R1GG0>
2022-05-05 19:50:10 +0530

    5maydemo2

---
+++ E2EMavenGitJenkinsIntegration/src/MavenGitJenkins/TC_001.java
@@ -0,0 +1,24 @@
+package MavenGitJenkins;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void testCase1() {
+		
+	
+		System.setProperty("webdriver.chrome.driver", "C://browserdrivers//chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.findElement(By.cssSelector("input#txtUsername")).sendKeys("Admin");
+		driver.findElement(By.cssSelector("input#txtPassword")).sendKeys("admin123");
+		driver.findElement(By.cssSelector("input.button")).click();
+		driver.quit();
+		
+	}
+
+}
---
+++ E2EMavenGitJenkinsIntegration/src/MavenGitJenkins/TC_002.java
@@ -0,0 +1,23 @@
+package MavenGitJenkins;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+	
+	@Test
+	public void testCase2() {
+		
+	
+		System.setProperty("webdriver.chrome.driver", "C://browserdrivers//chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.findElement(By.cssSelector("input#txtUsername")).sendKeys("Admin");
+		driver.findElement(By.cssSelector("input#txtPassword")).sendKeys("admin123");
+		driver.findElement(By.cssSelector("input.button")).click();
+		driver.quit();
+
+}
+}
